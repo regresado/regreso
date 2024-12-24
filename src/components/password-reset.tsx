7d11efeedf7870eec904cd5f180400d3f7ea0a6a
Josh Pinto <joshpinto6@outlook.com>
2024-12-23 22:08:09 -0500

    :fire: Remove unused import

--- src/components/password-reset.tsx
+++ src/components/password-reset.tsx
@@ -2,8 +2,6 @@
 
 import { useActionState, useEffect } from "react";
 
-import Link from "next/link";
-
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
