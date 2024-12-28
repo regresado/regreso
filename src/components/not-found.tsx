7ef539ef5e330c23217917625c3aa51b5baef105
Josh Pinto <joshpinto6@outlook.com>
2024-12-27 19:35:11 -0500

    :ambulance: Fix motion/react imports

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 import Link from "next/link";
 
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { Footprints } from "lucide-react";
 
 import { Button } from "~/components/ui/button";
