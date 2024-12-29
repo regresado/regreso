b4be8f4ba3c6d2ea5f5e024fc07f8c8cb32af9c6
Josh Pinto <joshpinto6@outlook.com>
2024-12-29 17:51:13 -0500

    :lipstick: Add Tilt Card to destination and welcome cards

--- src/app/(platform)/_components/destination.tsx
+++ src/app/(platform)/_components/destination.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+
 import { MinimalTiptapEditor } from "~/components/minimal-tiptap";
 import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
@@ -24,9 +25,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "~/components/ui/select";
+import { TiltCard } from "~/components/tilt-card";
 import { api } from "~/trpc/react";
 import { type Tag, TagInput } from "emblor";
-import { ArrowRight, MapPin, MapPinPlus } from "lucide-react";
+import { Plus, ArrowRight, MapPin, MapPinPlus, RefreshCcw } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { type z } from "zod";
 import { destinationSchema, type Destination } from "~/server/models";
@@ -59,162 +61,169 @@ export function CreateDestination() {
   const [locationConfirmed, confirmLocation] = useState(false);
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="flex items-center">
-          <MapPinPlus className="mr-2 w-5" /> Create Destination
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="sm:px-3 xl:px-6">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <div
-              className={`flex flex-row items-end xs:space-x-1 xl:space-x-3`}
-            >
+    <TiltCard>
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center">
+            <MapPinPlus className="mr-2 w-5" /> Create Destination
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="sm:px-3 xl:px-6">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
               <div
-                className={`w-${form.watch("type") === "location" ? "1/3" : "full"}`}
+                className={`flex flex-row items-end xs:space-x-1 xl:space-x-3`}
               >
-                <FormField
-                  control={form.control}
-                  name="type"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Type</FormLabel>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select a type" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value="location">Location</SelectItem>
-                          <SelectItem value="note">Note</SelectItem>
-                          <SelectItem value="file" disabled>
-                            File
-                            <Badge variant="secondary" className="ml-2">
-                              Soon!
-                            </Badge>
-                          </SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              {form.watch("type") == "location" ? (
-                <>
-                  <div className="w-1/2">
-                    <FormField
-                      control={form.control}
-                      name="location"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Location</FormLabel>
+                <div
+                  className={`w-${form.watch("type") === "location" ? "1/3" : "full"}`}
+                >
+                  <FormField
+                    control={form.control}
+                    name="type"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Type</FormLabel>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
                           <FormControl>
-                            <Input
-                              placeholder="https://pelicans.dev"
-                              {...field}
-                            />
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select a type" />
+                            </SelectTrigger>
                           </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  <Button
-                    size="icon"
-                    disabled={form.watch("location") === ""}
-                    onClick={() => {
-                      confirmLocation(true);
-                    }}
-                  >
-                    <ArrowRight />
-                  </Button>
+                          <SelectContent>
+                            <SelectItem value="location">Location</SelectItem>
+                            <SelectItem value="note">Note</SelectItem>
+                            <SelectItem value="file" disabled>
+                              File
+                              <Badge variant="secondary" className="ml-2">
+                                Soon!
+                              </Badge>
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                {form.watch("type") == "location" ? (
+                  <>
+                    <div className="w-1/2">
+                      <FormField
+                        control={form.control}
+                        name="location"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Location</FormLabel>
+                            <FormControl>
+                              <Input
+                                placeholder="https://pelicans.dev"
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <Button
+                      size="icon"
+                      disabled={form.watch("location") === ""}
+                      onClick={() => {
+                        confirmLocation(true);
+                      }}
+                    >
+                      <ArrowRight />
+                    </Button>
+                  </>
+                ) : null}
+              </div>
+              {form.watch("type") === "note" ||
+              (form.watch("type") === "location" && locationConfirmed) ? (
+                <>
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Headline</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Coolest Pelicans in the World"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="body"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Headline</FormLabel>
+                        <FormControl>
+                          <MinimalTiptapEditor
+                            value={field.value}
+                            onChange={field.onChange}
+                            className="w-full"
+                            editorContentClassName="p-5"
+                            output="html"
+                            placeholder="Type your note here..."
+                            editable={true}
+                            editorClassName="focus:outline-none"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="tags"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col items-start">
+                        <FormLabel className="text-left">Tags</FormLabel>
+                        <FormControl>
+                          <TagInput
+                            {...field}
+                            placeholder="Enter some tags..."
+                            tags={tags}
+                            className="sm:min-w-[450px]"
+                            setTags={(newTags) => {
+                              setTags(newTags);
+                              form.setValue("tags", newTags as [Tag, ...Tag[]]);
+                            }}
+                            activeTagIndex={-1}
+                            setActiveTagIndex={() => {}}
+                          />
+                        </FormControl>
+                        <FormDescription>
+                          These are the topics that you&apos;re interested in.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                 </>
               ) : null}
-            </div>
-            {form.watch("type") === "note" ||
-            (form.watch("type") === "location" && locationConfirmed) ? (
-              <>
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Headline</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="Coolest Pelicans in the World"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="body"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Headline</FormLabel>
-                      <FormControl>
-                        <MinimalTiptapEditor
-                          value={field.value}
-                          onChange={field.onChange}
-                          className="w-full"
-                          editorContentClassName="p-5"
-                          output="html"
-                          placeholder="Type your note here..."
-                          editable={true}
-                          editorClassName="focus:outline-none"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="tags"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-col items-start">
-                      <FormLabel className="text-left">Tags</FormLabel>
-                      <FormControl>
-                        <TagInput
-                          {...field}
-                          placeholder="Enter some tags..."
-                          tags={tags}
-                          className="sm:min-w-[450px]"
-                          setTags={(newTags) => {
-                            setTags(newTags);
-                            form.setValue("tags", newTags as [Tag, ...Tag[]]);
-                          }}
-                          activeTagIndex={-1}
-                          setActiveTagIndex={() => {}}
-                        />
-                      </FormControl>
-                      <FormDescription>
-                        These are the topics that you&apos;re interested in.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </>
-            ) : null}
-            <Button type="submit" disabled={createDestination.isPending}>
-              {createDestination.isPending ? "Submitting..." : "Submit "}
-            </Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+              <Button
+                type="submit"
+                disabled={createDestination.isPending}
+                size="sm"
+              >
+                <Plus />
+                {createDestination.isPending ? "Submitting..." : "Submit "}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </TiltCard>
   );
 }
 
@@ -222,24 +231,30 @@ export function RecentDestinations() {
   const [recentDestinations] = api.destination.getRecent.useSuspenseQuery();
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="flex items-center">
-          <MapPin className="mr-2 w-5" /> Recent Destinations
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="sm:px-3 xl:px-6">
-        {recentDestinations.length > 0 ? (
-          recentDestinations.map((dest: Destination) => {
-            return <DestinationCard {...dest} />;
-          })
-        ) : (
-          <p className="text-sm text-muted-foreground">
-            🌌 No destinations found. Try creating one and come back!
-          </p>
-        )}
-      </CardContent>
-    </Card>
+    <TiltCard>
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center">
+            <MapPin className="mr-2 w-5" /> Recent Destinations
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-6 sm:px-3 xl:px-6">
+          {recentDestinations.length > 0 ? (
+            recentDestinations.map((dest: Destination) => {
+              return <DestinationCard {...dest} />;
+            })
+          ) : (
+            <p className="text-sm text-muted-foreground">
+              🌌 No destinations found. Try creating one and come back!
+            </p>
+          )}
+          <Button size="sm">
+            <RefreshCcw />
+            Refresh
+          </Button>
+        </CardContent>
+      </Card>
+    </TiltCard>
   );
 }
 
