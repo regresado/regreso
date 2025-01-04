87fb386d299f212b74a39239ae15efd63cc430d0
Josh Pinto <joshpinto6@outlook.com>
2025-01-03 20:04:20 -0500

    :card_file_box: Add lists, updated search index to DB

--- setup.sql
+++ setup.sql
@@ -25,6 +25,20 @@ CREATE TABLE "regreso_email_verification_request" (
 	"expires_at" timestamp with time zone NOT NULL
 );
 
+CREATE TABLE "regreso_list_tag" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"list_id" integer,
+	"tag_id" integer,
+	CONSTRAINT "regreso_list_tag_list_id_tag_id_unique" UNIQUE("list_id","tag_id")
+);
+
+CREATE TABLE "regreso_list" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256),
+	"user_id" integer NOT NULL,
+	CONSTRAINT "regreso_list_user_id_name_unique" UNIQUE("user_id","name")
+);
+
 CREATE TABLE "regreso_passkey_credential" (
 	"id" text PRIMARY KEY NOT NULL,
 	"user_id" integer NOT NULL,
@@ -96,18 +110,19 @@ ALTER TABLE "regreso_destination_tag" ADD CONSTRAINT "regreso_destination_tag_de
 ALTER TABLE "regreso_destination_tag" ADD CONSTRAINT "regreso_destination_tag_tag_id_regreso_tag_id_fk" FOREIGN KEY ("tag_id") REFERENCES "public"."regreso_tag"("id") ON DELETE cascade ON UPDATE cascade;
 ALTER TABLE "regreso_destination" ADD CONSTRAINT "regreso_destination_user_id_regreso_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."regreso_user"("id") ON DELETE no action ON UPDATE no action;
 ALTER TABLE "regreso_email_verification_request" ADD CONSTRAINT "regreso_email_verification_request_user_id_regreso_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."regreso_user"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "regreso_list_tag" ADD CONSTRAINT "regreso_list_tag_list_id_regreso_list_id_fk" FOREIGN KEY ("list_id") REFERENCES "public"."regreso_list"("id") ON DELETE cascade ON UPDATE cascade;
+ALTER TABLE "regreso_list_tag" ADD CONSTRAINT "regreso_list_tag_tag_id_regreso_tag_id_fk" FOREIGN KEY ("tag_id") REFERENCES "public"."regreso_tag"("id") ON DELETE cascade ON UPDATE cascade;
+ALTER TABLE "regreso_list" ADD CONSTRAINT "regreso_list_user_id_regreso_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."regreso_user"("id") ON DELETE no action ON UPDATE no action;
 ALTER TABLE "regreso_passkey_credential" ADD CONSTRAINT "regreso_passkey_credential_user_id_regreso_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."regreso_user"("id") ON DELETE no action ON UPDATE no action;
 ALTER TABLE "regreso_password_reset_session" ADD CONSTRAINT "regreso_password_reset_session_user_id_regreso_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."regreso_user"("id") ON DELETE no action ON UPDATE no action;
 ALTER TABLE "regreso_security_key_credential" ADD CONSTRAINT "regreso_security_key_credential_user_id_regreso_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."regreso_user"("id") ON DELETE no action ON UPDATE no action;
 ALTER TABLE "regreso_session" ADD CONSTRAINT "regreso_session_user_id_regreso_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."regreso_user"("id") ON DELETE no action ON UPDATE no action;
 ALTER TABLE "regreso_tag" ADD CONSTRAINT "regreso_tag_user_id_regreso_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."regreso_user"("id") ON DELETE no action ON UPDATE no action;
 ALTER TABLE "regreso_totp_credential" ADD CONSTRAINT "regreso_totp_credential_user_id_regreso_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."regreso_user"("id") ON DELETE no action ON UPDATE no action;
-CREATE INDEX "destination_search_index" ON "regreso_destination" USING gin (
-    to_tsvector('english', coalesce("name", '') || ' ' || coalesce("body", ''))
-);
-CREATE INDEX "tag_search_index" ON "regreso_tag" USING gin (
-    to_tsvector('english', coalesce("shortcut", '') || ' ' || coalesce("name", ''))
-);
+CREATE INDEX "destination_search_index" ON "regreso_destination" USING gin ((setweight(to_tsvector('english', "name"), 'A') ||
+          setweight(to_tsvector('english', "body"), 'B')));
+CREATE INDEX "tag_search_index" ON "regreso_tag" USING gin ((setweight(to_tsvector('english', "shortcut"), 'A') ||
+          setweight(to_tsvector('english', "name"), 'B')));
 CREATE INDEX "email_index" ON "regreso_user" USING btree ("email");
 CREATE INDEX "google_id_index" ON "regreso_user" USING btree ("google_id");
 CREATE INDEX "github_id_index" ON "regreso_user" USING btree ("github_id");
