e1c5e7337cd4d5d530f542027ee93230596743d3
Josh Pinto <joshpinto6@outlook.com>
2025-01-19 10:17:54 -0500

    :bug: Fix not found component on blog posts

--- src/app/(knowledge)/blog/[slug]/page.tsx
+++ src/app/(knowledge)/blog/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { getPostData, getSortedPostsData } from "~/lib/knowledge";
 
 import { Button } from "~/components/ui/button";
+import NotFound from "~/components/not-found";
 
 import { SiteContent } from "../../_components/content";
 
@@ -20,7 +21,10 @@ export default async function Post({
   params: Promise<{ slug: string }>;
 }) {
   const postData = await getPostData((await params).slug);
-  return (
+
+  return postData === "not found" ? (
+    <NotFound />
+  ) : (
     <div className="align-center min-w-sm mx-auto max-w-3xl space-y-8 pt-4">
       <SiteContent postData={postData} />
       <footer>
