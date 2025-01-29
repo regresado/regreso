974210234ebba6045329e3f5d949ec5071becd9e
Joshpinto6 <91392083+joshpinto6@users.noreply.github.com>
2025-01-29 19:02:50 +0000

    :bug: Fix Tolgee

--- src/tolgee/shared.ts
+++ src/tolgee/shared.ts
@@ -8,29 +8,23 @@ export const ALL_LANGUAGES = ["en"];
 export const DEFAULT_LANGUAGE = "en";
 
 export function TolgeeBase() {
-  return Tolgee()
-    .use(FormatSimple())
-    .use(
-      BackendFetch({
-        prefix: "https://cdn.tolg.ee/d7df50a9ae876ad51f160bc70eb7073a",
-        next: {
-          revalidate: 60, // cache CDN data for one minute
+  return (
+    Tolgee()
+      .use(FormatSimple())
+      // .use(process.env.NODE_ENV == "development" ? DevTools() : undefined)
+      // replace with .use(FormatIcu()) for rendering plurals, foramatted numbers, etc.
+      // .updateDefaults({
+      //   apiKey: apiKey ?? undefined,
+      //   apiUrl: apiUrl ?? undefined,
+      // })
+      .updateDefaults({
+        apiKey,
+        apiUrl,
+        staticData: {
+          en: () => import("../../messages/en.json"),
+          "en:Navigation": () => import("../../messages/Navigation/en.json"),
+          "en:LandingPage": () => import("../../messages/LandingPage/en.json"),
         },
-      }),
-    );
-  // .use(process.env.NODE_ENV == "development" ? DevTools() : undefined)
-  // replace with .use(FormatIcu()) for rendering plurals, foramatted numbers, etc.
-  // .updateDefaults({
-  //   apiKey: apiKey ?? undefined,
-  //   apiUrl: apiUrl ?? undefined,
-  // })
-  // .updateDefaults({
-  //   // apiKey,
-  //   // apiUrl,
-  //   staticData: {
-  //     en: () => import("../../messages/en.json"),
-  //     "en:Navigation": () => import("../../messages/Navigation/en.json"),
-  //     "en:LandingPage": () => import("../../messages/LandingPage/en.json"),
-  //   },
-  // })
+      })
+  );
 }
